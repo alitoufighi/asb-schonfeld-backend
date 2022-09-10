1d2e09daf65d3922ebd0f6a5d2f9bbd0bc22bdd9
Mohammad Ali Tofighi <alitoumob@gmail.com>
2022-09-10 11:47:39 -0400

    Remove frontend project

--- backend/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build --no-cache -t dineshsonachalam/tech-courses-search-engine-backend:latest .
-docker push dineshsonachalam/tech-courses-search-engine-backend:latest
--- frontend/src/redux/reducers/index.js
+++
@@ -1,7 +0,0 @@
-// Step 3: Create reducers for the action types
-
-import { combineReducers } from "redux";
-
-import searchReducer from "./searchReducer";
-
-export default combineReducers({ searchReducer });
