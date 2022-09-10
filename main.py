15ff79fe6ba2aef0d13026bff4a39d6f80060990
Mohammad Ali Tofighi <alitoumob@gmail.com>
2022-09-10 17:45:25 -0400

    Remove calling precondition check

--- main.py
+++ main.py
@@ -12,21 +12,15 @@ es = Elasticsearch(index="asb.fiu")
 
 @app.get("/autocomplete")
 async def autocomplete(query: str = ""):
-    if(es.pre_condition_check()):
-        result = search.autocomplete(query=query)
-        return result
-    else:
-        return []
+    result = search.autocomplete(query=query)
+    return result
 
 @app.post("/search")
 async def string_query_seach(query: str = ""):
     query = unquote_plus(query)
     print(query)
-    if(es.pre_condition_check()):
-        result = search.string_query_search(query=query)
-        return result
-    else:
-        return []
+    result = search.string_query_search(query=query)
+    return result
 
 
 if __name__ == "__main__":
